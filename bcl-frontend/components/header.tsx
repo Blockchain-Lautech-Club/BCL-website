23e5610ff9f90dda538538dd40a3e5d8d1dee33b
DIFoundation <adeniranibrahim165@gmail.com>
2026-06-07 20:32:29 +0100

    optimize header

--- bcl-frontend/components/header.tsx
+++ bcl-frontend/components/header.tsx
@@ -1,14 +1,15 @@
 'use client';
 import { useState } from "react";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Menu, X } from "lucide-react";
-import bclLogo from "../public/logos/logoNoBackground.png";
 import { motion } from 'framer-motion';
 import { organization, navigation } from '@/lib/data.json'
 
 export function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
 
   const navVariants = {
     hidden: { opacity: 0, y: -20 },
@@ -25,35 +26,52 @@ export function Header() {
       variants={navVariants}
       initial="hidden"
       animate="visible"
-      className="bg-white/95 backdrop-blur-sm border-b border-gray-200 sticky top-0 z-50"
+      className="bg-white/55 backdrop-blur-sm border-b border-gray-200 sticky top-0 z-50"
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
+        {/* Desktop navigation */}
+        <div className="justify-between items-center h-16 hidden md:flex">
           <div className="flex items-center">
             <Link href="/">
               <img className="h-12 w-auto" src={organization.logos[1]} alt="Blockchain Lautech Logo" />
             </Link>
           </div>
 
-          <div className="hidden md:flex items-center space-x-4">
+          <div className="flex justify-center items-center space-x-8">
             {navigation.headerLinks.map((link) => (
               <Link
                 key={link.label}
                 href={link.url}
-                className="text-gray-900 hover:text-primary py-2 text-lg font-bold transition-colors duration-200"
+                className={`py-2 text-lg transition-colors duration-200 ${pathname === link.url ? 'text-primary font-bold' : 'text-gray-900 hover:text-primary'}`}
               >
                 {link.label}
               </Link>
             ))}
+          </div>
 
-            {navigation.ctaButtons.map((button) => (
-              <Button asChild key={button.label} className="ml-3 text-md font-bold bg-primary hover:bg-primary/90">
+          <div className="flex justify-center items-center space-x-2">
+            {navigation.ctaButtons.map((button, index) => (
+              <Button
+                asChild
+                key={button.label}
+                variant={index === 1 ? "outlineCta" : "default"}
+                className="ml-3 text-md font-bold"
+              >
                 <Link href={button.url}>{button.label}</Link>
               </Button>
             ))}
           </div>
+        </div>
 
-          <div className="md:hidden">
+        {/* Mobile navigation */}
+        <div className="flex justify-between items-center h-16 md:hidden">
+          <div className="flex items-center">
+            <Link href="/">
+              <img className="h-12 w-auto" src={organization.logos[1]} alt="Blockchain Lautech Logo" />
+            </Link>
+          </div>
+
+          <div className="">
             <Button
               variant="ghost"
               size="sm"
@@ -79,15 +97,15 @@ export function Header() {
                   <Link
                     key={link.label}
                     href={link.url}
-                    className="block px-3 py-2 text-md font-bold text-gray-900 hover:text-primary transition-colors"
+                    className={`block px-3 py-2 text-md transition-colors ${pathname === link.url ? 'font-bold text-primary' : 'text-gray-900 hover:text-primary'}`}
                     onClick={() => setIsOpen(false)}
                   >
                     {link.label}
                   </Link>
                 ))}
-                {navigation.ctaButtons.map((button) => (
+                {navigation.ctaButtons.map((button, index) => (
                   <div key={button.label} className="px-3 py-2">
-                    <Button asChild className="w-full text-md font-bold bg-primary hover:bg-primary/90">
+                    <Button asChild variant={index === 1 ? "outlineCta" : "default"} className="w-full text-md font-bold">
                       <Link href={button.url} onClick={() => setIsOpen(false)}>{button.label}</Link>
                     </Button>
                   </div>
